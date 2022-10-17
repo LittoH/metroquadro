01603f3819630fc1db611f329989b2d3df53ce2e
LittoH <francesco.littarru209@gmail.com>
2022-10-17 03:40:05 +0200

    Fixed navbar changing background based on position

--- metroquadro-frontend/components/navbar.js
+++ metroquadro-frontend/components/navbar.js
@@ -1,10 +1,35 @@
 import { useRouter } from 'next/router';
-import styles from '../styles/Navbar.module.scss';
+import styles from '../styles/Navbar.module.css';
 import { Navlink } from '../components/navlink';
+import { useEffect } from 'react';
 
 export const Navbar = () => {
 
     const router = useRouter();
+
+    // Parte necessaria per il cambio di sfondo della navbar
+    const debounce = (fn) => {
+        let frame;
+        return (...params) => {
+            if (frame) { 
+                cancelAnimationFrame(frame);
+            }
+            frame = requestAnimationFrame(() => {
+                fn(...params);
+            });
+        } 
+    };
+  
+    const storeScroll = () => {
+        document.documentElement.dataset.scroll = window.scrollY;
+    }
+
+    useEffect(() => {
+        document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+        storeScroll();
+    });
+
+    // Fine parte necessaria per il cambio di sfondo della navbar
 
     return (
         <div id="navbarContainer" className={styles.container}>
