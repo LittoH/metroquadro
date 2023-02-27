78ada597a93196358435c20f463fcef8ff32c03c
LittoH <francesco.littarru209@gmail.com>
2023-02-27 23:54:50 +0100

    small changes to searchbar

--- metroquadro-frontend/app/searchbar.js
+++ metroquadro-frontend/app/searchbar.js
@@ -4,12 +4,9 @@ import CustomButton from './customButton'
 export default function SearchBar() {
     return (
         <>
-            <input className={styles.barBackground} type="search" id="searchbar" placeholder="Inserisci qua le parole chiave"></input>
-            <CustomButton
-                text="Filtri"
-                path="/immobili"
-                theme="dark"
-             />
+            <div className={styles.greenBarWrapper}>
+                <input className={styles.searchBar} type="search" id="searchbar" placeholder="Inserisci qua le parole chiave"></input>
+            </div>
         </>
     ) 
 }
