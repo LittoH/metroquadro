d3cd9f70761a6752222cec37f697bb33ce7345b0
LittoH <francesco.littarru209@gmail.com>
2023-03-25 21:41:39 +0100

    further development on immobili page

--- metroquadro-frontend/app/searchbar.js
+++ metroquadro-frontend/app/searchbar.js
@@ -1,13 +1,86 @@
 import styles from '../styles/Searchbar.module.css';
+import { useState } from 'react';
+import Select from 'react-select';
+
+const optionsComuni = [
+    { value: 'Ostiglia', label: 'Ostiglia' },
+    { value: 'Mantova', label: 'Mantova' },
+    { value: 'Cittadella', label: 'Cittadella' },
+];
+
+const optionsContratto = [
+    { value: 'Vendita', label: 'Vendita' },
+    { value: 'Affitto', label: 'Affitto' },
+];
+
+const optionsTipologia = [
+    { value: 'Casa', label: 'Casa' },
+    { value: 'Appartamento', label: 'Appartamento' },
+];
+
+const optionsPrezzo = [
+    { value: '10000', label: '€10.000' },
+    { value: '20000', label: '€20.000' },
+    { value: '30000', label: '€30.000' },
+    { value: '40000', label: '€40.000' },
+    { value: '50000', label: '€50.000' },
+    { value: '60000', label: '€60.000' },
+    { value: '70000', label: '€70.000' },
+    { value: '80000', label: '€80.000' },
+    { value: '90000', label: '€90.000' },
+    { value: '100000', label: '€100.000' },
+    { value: '120000', label: '€120.000' },
+    { value: '140000', label: '€140.000' },
+    { value: '160000', label: '€160.000' },
+    { value: '180000', label: '€180.000' },
+    { value: '200000', label: '€200.000' },
+    { value: '220000', label: '€220.000' },
+    { value: '240000', label: '€240.000' },
+    { value: '260000', label: '€260.000' },
+    { value: '280000', label: '€280.000' },
+    { value: '300000', label: '€300.000' },
+    { value: '320000', label: '€320.000' },
+    { value: '340000', label: '€340.000' },
+    { value: '360000', label: '€360.000' },
+    { value: '380000', label: '€380.000' },
+    { value: '400000', label: '€400.000' },
+    { value: '450000', label: '€450.000' },
+    { value: '500000', label: '€500.000' },
+    { value: '550000', label: '€550.000' },
+    { value: '600000', label: '€600.000' },
+];
+
+const optionsSuperficie = [
+    { value: '20', label: '20m²' },
+    { value: '40', label: '40m²' },
+    { value: '60', label: '60m²' },
+    { value: '80', label: '80m²' },
+    { value: '100', label: '100m²' },
+    { value: '120', label: '120m²' },
+    { value: '140', label: '140m²' },
+    { value: '160', label: '160m²' },
+    { value: '180', label: '180m²' },
+    { value: '200', label: '200m²' },
+    { value: '250', label: '250m²' },
+    { value: '300', label: '300m²' },
+    { value: '350', label: '350m²' },
+    { value: '400', label: '400m²' },
+    { value: '450', label: '450m²' },
+    { value: '500', label: '500m²' },
+];
 
 export default function SearchBar({ searchFilter, setSearchFilter }) {
 
-    function LoadCaseMantova() {
-        setSearchFilter("&filter=(comune='Mantova')");
-    }
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [selectedOptionContratto, setSelectedOptionContratto] = useState(null);
+    const [selectedOptionTipologia, setSelectedOptionTipologia] = useState(null);
+    const [selectedOptionPrezzo, setSelectedOptionPrezzo] = useState(null);
+    const [selectedOptionSuperficie, setSelectedOptionSuperficie] = useState(null);
 
-    function LoadCaseOstiglia() {
-        setSearchFilter("&filter=(comune='Ostiglia')");
+    function LoadCaseWithFilter() {
+        const filter = "&filter=(comune='" + selectedOption.value + "')";
+        console.log(filter);
+        setSearchFilter(filter);
     }
 
     function LoadTutteCaseAttive() {
@@ -17,13 +90,45 @@ export default function SearchBar({ searchFilter, setSearchFilter }) {
     return (
         <>
             <div className={styles.greenBarWrapper}>
-                <input className={styles.searchBar} type="search" id="searchbar" placeholder="Inserisci qua le parole chiave"></input>
-                <div className={styles.buttonsWrapper}>
-                    <div className={styles.searchBarButton} onClick={() => LoadCaseMantova()} >Filtra per Mantova</div>
-                    <div className={styles.searchBarButton} onClick={() => LoadCaseOstiglia()} >Filtra per Ostiglia</div>
-                    <div className={styles.searchBarButton} onClick={() => LoadTutteCaseAttive()} >Reset filtri</div>
+                <div style={{ display: "flex", flexDirection: "row", columnGap: "10px" }}>
+                    <Select
+                        defaultValue={selectedOption}
+                        onChange={setSelectedOption}
+                        options={optionsComuni}
+                        placeholder={"Comune"}
+                    />
+                    <Select
+                        defaultValue={selectedOptionContratto}
+                        onChange={setSelectedOptionContratto}
+                        options={optionsContratto}
+                        placeholder={"Contratto"}
+                    />
+                    <Select
+                        defaultValue={selectedOptionTipologia}
+                        onChange={setSelectedOptionTipologia}
+                        options={optionsTipologia}
+                        placeholder={"Tipologia immobile"}
+                    />
+                    <Select
+                        defaultValue={selectedOptionPrezzo}
+                        onChange={setSelectedOptionPrezzo}
+                        options={optionsPrezzo}
+                        placeholder={"Prezzo"}
+                    />
+                    <Select
+                        defaultValue={selectedOptionSuperficie}
+                        onChange={setSelectedOptionSuperficie}
+                        options={optionsSuperficie}
+                        placeholder={"Superficie"}
+                    />
+                    <div className={styles.searchBarButton} onClick={() => LoadCaseWithFilter()}>Cerca</div>
+                    <div className={styles.searchBarButton} onClick={() => LoadTutteCaseAttive()}>Reset filtri</div>
                 </div>
+                
             </div>
         </>
     )
 }
+
+// <input className={styles.searchBar} type="search" id="searchbar" placeholder="Inserisci qua le parole chiave"></input>
+// <div className={styles.buttonsWrapper}></div>
