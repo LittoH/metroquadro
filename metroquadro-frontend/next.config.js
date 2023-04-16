1c84d1bf077391abec60b87ec37c68be3b069dcf
LittoH <francesco.littarru209@gmail.com>
2023-04-16 17:47:15 +0200

    tentative fix for homepage form not working correctly

--- metroquadro-frontend/next.config.js
+++ metroquadro-frontend/next.config.js
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {appDir: true},
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: '**',
-            port: '',
-            pathname: '**',
-        },
-    ],
-},
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: { appDir: true },
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
+
+    env: {
+        NETLIFY_NEXT_PLUGIN_SKIP: true,
+      },
 
 }
 
