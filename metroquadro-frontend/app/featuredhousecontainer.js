2b25570c9b23290b221d0eab6dd3636f21130549
LittoH <francesco.littarru209@gmail.com>
2023-04-16 21:20:37 +0200

    Now multifeaturedhouse component displays arrows to scroll it

--- metroquadro-frontend/app/featuredhousecontainer.js
+++ metroquadro-frontend/app/featuredhousecontainer.js
@@ -1,42 +1,95 @@
+'use client'
+
 import styles from '../styles/Featuredhousecontainer.module.css'
 import FeaturedHouseCard from './featuredhousecard'
 import Image from 'next/image';
+import PocketBase from 'pocketbase';
+import { useState, useEffect } from 'react';
+import { useRef } from 'react';
 
-export default async function FeaturedHouseContainer() {
-    
-    const res = await fetch('https://metroquadro-backend-production.up.railway.app/api/collections/immobili_vendita/records?page=1&perPage=30',
-        { cache: 'no-store' }
-    );
-    const data = await res.json();
-    
-    let houses = data.items;
+export default function FeaturedHouseContainer() {
+
+    const [houses, setHouses] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasScrolled, setHasScrolled] = useState(false);
+
+    // Questo ci serve per ottenere la reference del div da scrollare orizzontalmente quando
+    // clicchiamo sull'icona della freccia
+    const divRef = useRef(null);
+
+    // Inizio parte relativa al fetch dal lato client 
+    const client = new PocketBase('https://metroquadro-backend-production.up.railway.app');
+    let searchFilter = "";
+
+    const fetchFilteredImmobili = async () => {
+        const response = await client.collection('immobili_vendita').getList(1, 50, {
+            filter: '' + searchFilter,
+        });
+        setHouses(response.items);
+        setIsLoading(false);
+        return response.items;
+    }
+
+    useEffect(() => {
+        fetchFilteredImmobili();
+    }, [])
+
+    // Fine parte relativa al fetch dal lato client 
+
+    // Aggiungo degli event listeners per capire se ho scrollato orizzontalmente lo slider
+    useEffect(() => {
+        const scrollableDiv = document.querySelector('#scrollableDiv');
+        const handleScroll = () => {
+            if (scrollableDiv.scrollLeft > 0) {
+                setHasScrolled(true);
+            } else {
+                setHasScrolled(false);
+            }
+        };
+        scrollableDiv.addEventListener('scroll', handleScroll);
+        return () => {
+            scrollableDiv.removeEventListener('scroll', handleScroll);
+        };
+    }, [hasScrolled]);
+
 
     function ScrollRight() {
-        console.log("Scrolled to the right");
+        divRef.current.scrollLeft += 600;
+    }
+
+    function ScrollLeft() {
+        divRef.current.scrollLeft -= 600;
     }
 
     return (
-        <div className={styles.multiFeaturedHouseContainer}>
-            {/* 
-            <div className={styles.rightArrow} onClick={() => ScrollRight()}> 
-            <Image className={styles.rightArrowImage} src="/nav-arrow-right.svg" width={10} height={10} alt="Right arrow" />
+        <div className={styles.multiFeaturedHouseWrapper}>
+            <div className={styles.rightArrow} onClick={() => ScrollRight()}>
+                <Image className={styles.rightArrowImage} src="/nav-arrow-right.svg" width={50} height={50} alt="Right arrow" />
             </div>
-            */}
-            {houses.filter(house => house.attivo && house.in_evidenza).map((house) => (
-                <>
-                    <FeaturedHouseCard 
-                        id={house.id}
-                        imgSrc={house.immagine} 
-                        price={house.prezzo} 
-                        title={house.titolo} 
-                        numLocales={house.locali} 
-                        m2={house.metriquadri} 
-                        bathrooms={house.bagni}
-                        floors={house.piani} 
-                        description={house.annuncio}
-                    />
-                </>
-            ))}
+
+            {hasScrolled && 
+                <div className={styles.leftArrow} onClick={() => ScrollLeft()}>
+                    <Image className={styles.rightArrowImage} src="/nav-arrow-left.svg" width={50} height={50} alt="Right arrow" />
+                </div>
+            }
+
+            <div id="scrollableDiv" ref={divRef} className={styles.multiFeaturedHouseContainer}>
+                {!isLoading && houses.filter(house => house.attivo && house.in_evidenza).map((house) => (
+                    <>
+                        <FeaturedHouseCard
+                            id={house.id}
+                            imgSrc={house.immagine}
+                            price={house.prezzo}
+                            title={house.titolo}
+                            numLocales={house.locali}
+                            m2={house.metriquadri}
+                            bathrooms={house.bagni}
+                            floors={house.piani}
+                            description={house.annuncio}
+                        />
+                    </>
+                ))}
+            </div>
         </div>
     );
 };
