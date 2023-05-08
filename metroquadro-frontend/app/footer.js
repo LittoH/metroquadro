aa27ad151f6e21551d1b305ea46bfa34aee0a40a
LittoH <francesco.littarru209@gmail.com>
2023-05-08 23:36:33 +0200

    Footer updated

--- metroquadro-frontend/app/footer.js
+++ metroquadro-frontend/app/footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
                         <Link href="/">
                             <Image className={styles.social} src="/tik-tok.svg" width={20} height={20} alt="Tik-Tok logo" />
                         </Link>
-                        <Link href="/">
+                        <Link href="https://www.instagram.com/agenziametroquadro/">
                             <Image style={{marginTop: "15px"}} className={styles.social} src="/instagram.svg" width={20} height={20} alt="Instagram logo" />
                         </Link>
                     </div>
