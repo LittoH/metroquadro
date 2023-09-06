c280c7ffba970cbc1f66cc98cfd93bf58f7dafe0
LittoH <francesco.littarru209@gmail.com>
2023-09-06 18:38:25 +0200

    added text-align: justify

--- metroquadro-frontend/app/consulenze/page.jsx
+++ metroquadro-frontend/app/consulenze/page.jsx
@@ -1,6 +1,7 @@
 import styles from '../../styles/Vendi.module.css'
 import ScrollUp from '../scrollUp'
 import Herosection from '../herosection'
+//import { text } from 'stream/consumers'
 
 export default function ConsulenzePage() {
     return (
@@ -13,9 +14,9 @@ export default function ConsulenzePage() {
                         <div className={styles.contentItemsContainer}>
                             <div className={styles.contentItem} style={{ marginBottom: "100px" }}>
                                 <h1>Consulenze personalizzate per mutui e affitti</h1>
-                                <p>
+                                <p className={styles.justifiedText}>
                                     Sei interessato a comprare o affittare un immobile ma non sai se la tua disponibilità economica te lo concede? 
-                                    Per i nostri clienti offriamo un servizio di consulenza gratuita per analizzare la tua condizione economica in completo rispetto della privacy e trovare assieme il migliore compromesso per le tue necessità. <br></br>
+                                    Per i nostri clienti offriamo un servizio di consulenza gratuita per analizzare la tua condizione economica in completo rispetto della privacy e trovare assieme il migliore compromesso per le tue necessità. <br></br><br></br>
                                     Contattaci alla mail che segue per prendere appuntamento presso il nostro ufficio e pianificare una consulenza.
                                     Ti preghiamo di inserire nell'oggetto della mail [CONSULENZA] per aiutarci a meglio riconoscere la richiesta.<br></br>
                                     Sarai successivamente ricontattato direttamente alla mail che hai utilizzato per contattarci o al numero di telefono che lascerai. <br></br> <br></br><br></br>
