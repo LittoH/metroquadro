cc0a5a7d79c30517b9885e57d7a1fd70bc4da976
LittoH <francesco.littarru209@gmail.com>
2024-01-23 17:51:20 +0100

    Fixed slug page RC

--- metroquadro-frontend/app/immobili/[id]/page.jsx
+++ metroquadro-frontend/app/immobili/[id]/page.jsx
@@ -102,14 +102,14 @@ export default async function ImmobilePage({ params }) {
         <div className={styles.container}>
             <main className={styles.main}>
                 <section className={styles.houseInfoSection}>
-                    <div className={styles.customContainer} style={{ marginTop: "50px" }}>
+                    <div className={styles.customContainer}>
                         <div className={styles.planimetriaContainer}>
                             {photosList && (
                                 <PhotoCarouselLightbox carouselImages={photosList} carouselImagePath={imagesPath} carouselWidth="100%" carouselMaxHeight="75vh" carouselAspectRatio="4 / 3" />
                             )}
                         </div>
 
-                        <div className={styles.houseBigDetailItem} ><h1>{immobile.titolo}</h1></div>
+                        <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }} ><h1>{immobile.titolo}</h1></div>
                         <div className={styles.houseInfoOuterBriefDetailsContainer}>
                             <div className={styles.houseInfoInnerBriefDetailsContainer} style={{ paddingRight: "30px" }}>
                                 <div className={styles.houseDetailItem}><h3>Contratto</h3><p>{immobile.contratto}</p></div>
