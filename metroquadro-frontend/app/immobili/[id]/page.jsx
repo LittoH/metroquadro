0a7906e9d9c86cc935621ee0cc788acca73c76b6
LittoH <francesco.littarru209@gmail.com>
2024-03-06 19:03:24 +0100

    404 immobile page added

--- metroquadro-frontend/app/immobili/[id]/page.jsx
+++ metroquadro-frontend/app/immobili/[id]/page.jsx
@@ -1,6 +1,7 @@
 import styles from '../../../styles/Immobile.module.css';
 import HouseListContainer from '../../houselistcontainer';
 import Image from 'next/image';
+import CustomButton from '../../customButton';
 
 import PhotoCarouselLightbox from '../../photocarousellightbox';
 
@@ -102,135 +103,148 @@ export default async function ImmobilePage({ params }) {
         <div className={styles.container}>
             <main className={styles.main}>
                 <section className={styles.houseInfoSection}>
-                    <div className={styles.customContainer}>
-                        <div className={styles.planimetriaContainer}>
-                            {photosList && (
-                                <PhotoCarouselLightbox carouselImages={photosList} carouselImagePath={imagesPath} carouselWidth="100%" carouselMaxHeight="75vh" carouselAspectRatio="4 / 3" />
-                            )}
-                        </div>
-
-                        <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }} ><h1>{immobile.titolo}</h1></div>
-                        <div className={styles.houseInfoOuterBriefDetailsContainer}>
-                            <div className={styles.houseInfoInnerBriefDetailsContainer} style={{ paddingRight: "30px" }}>
-                                <div className={styles.houseDetailItem}><h3>Contratto</h3><p>{immobile.contratto}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Prezzo</h3><p>{immobile.prezzo}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Tipologia</h3><p>{immobile.tipologia_immobile}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Comune</h3><p>{immobile.comune}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Certificazione energetica</h3><p>{immobile.certificazione_energetica}</p></div>
+                    {immobile.attivo ? (
+                        <div className={styles.customContainer}>
+                            <div className={styles.planimetriaContainer}>
+                                {photosList && (
+                                    <PhotoCarouselLightbox carouselImages={photosList} carouselImagePath={imagesPath} carouselWidth="100%" carouselMaxHeight="75vh" carouselAspectRatio="4 / 3" />
+                                )}
                             </div>
 
-                            <div className={styles.houseInfoInnerBriefDetailsContainer} style={{ paddingLeft: "30px" }}>
-                                <div className={styles.houseDetailItem}><h3>Metriquadri:</h3><p>{immobile.metriquadri}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Numero locali: </h3><p>{immobile.locali}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Bagni:</h3><p>{immobile.bagni}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Piani:</h3><p>{immobile.piani}</p></div>
-                                <div className={styles.houseDetailItem}><h3>Garage:</h3><p>{immobile.garage}</p></div>
-                            </div>
-                        </div>
-
-                        <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Annuncio</h3><p>{immobile.annuncio}</p></div>
-                        <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Descrizione</h3><p>{immobile.descrizione}</p></div>
-
-                        <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Planimetria</h3></div>
-                        
-                        <div className={styles.planimetriaContainer}>
-                            {planimetrieList && (
-                                <PhotoCarouselLightbox carouselImages={planimetrieList} carouselImagePath={planimetriePath} carouselWidth="100%" carouselMaxHeight="75vh" carouselAspectRatio="4 / 3" />
-                            )}
-                        </div>
-
-                        <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Nelle vicinanze</h3></div>
-                        {nelleVicinanze && (
+                            <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }} ><h1>{immobile.titolo}</h1></div>
                             <div className={styles.houseInfoOuterBriefDetailsContainer}>
-
                                 <div className={styles.houseInfoInnerBriefDetailsContainer} style={{ paddingRight: "30px" }}>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Piscine</p>
-                                        {nelleVicinanze.piscine ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Scuole</p>
-                                        {nelleVicinanze.scuole ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Mezzi di trasporto</p>
-                                        {nelleVicinanze.mezzi_di_trasporto ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Palestra</p>
-                                        {nelleVicinanze.palestra ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Cibo da asporto</p>
-                                        {nelleVicinanze.cibo_da_asporto ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
+                                    <div className={styles.houseDetailItem}><h3>Contratto</h3><p>{immobile.contratto}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Prezzo</h3><p>{immobile.prezzo}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Tipologia</h3><p>{immobile.tipologia_immobile}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Comune</h3><p>{immobile.comune}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Certificazione energetica</h3><p>{immobile.certificazione_energetica}</p></div>
                                 </div>
 
                                 <div className={styles.houseInfoInnerBriefDetailsContainer} style={{ paddingLeft: "30px" }}>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Biblioteca</p>
-                                        {nelleVicinanze.biblioteca ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Supermercati</p>
-                                        {nelleVicinanze.supermercati ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Parchi</p>
-                                        {nelleVicinanze.parchi ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Campo sportivo</p>
-                                        {nelleVicinanze.campo_sportivo ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
-                                    <div className={styles.houseDetailItem}>
-                                        <p>Luoghi di culto</p>
-                                        {nelleVicinanze.luoghi_di_culto ? (
-                                            <Image src={"/check.svg"} width={20} height={20} />
-                                        ) : (
-                                            <Image src={"/cross.svg"} width={20} height={20} />
-                                        )}
-                                    </div>
+                                    <div className={styles.houseDetailItem}><h3>Metriquadri:</h3><p>{immobile.metriquadri}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Numero locali: </h3><p>{immobile.locali}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Bagni:</h3><p>{immobile.bagni}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Piani:</h3><p>{immobile.piani}</p></div>
+                                    <div className={styles.houseDetailItem}><h3>Garage:</h3><p>{immobile.garage}</p></div>
                                 </div>
                             </div>
-                                        )}
-                    </div>
+
+                            <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Annuncio</h3><p>{immobile.annuncio}</p></div>
+                            <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Descrizione</h3><p>{immobile.descrizione}</p></div>
+
+                            <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Planimetria</h3></div>
+
+                            <div className={styles.planimetriaContainer}>
+                                {planimetrieList && (
+                                    <PhotoCarouselLightbox carouselImages={planimetrieList} carouselImagePath={planimetriePath} carouselWidth="100%" carouselMaxHeight="75vh" carouselAspectRatio="4 / 3" />
+                                )}
+                            </div>
+
+                            <div className={styles.houseBigDetailItem} style={{ marginTop: "50px" }}><h3>Nelle vicinanze</h3></div>
+                            {nelleVicinanze && (
+                                <div className={styles.houseInfoOuterBriefDetailsContainer}>
+
+                                    <div className={styles.houseInfoInnerBriefDetailsContainer} style={{ paddingRight: "30px" }}>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Piscine</p>
+                                            {nelleVicinanze.piscine ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Scuole</p>
+                                            {nelleVicinanze.scuole ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Mezzi di trasporto</p>
+                                            {nelleVicinanze.mezzi_di_trasporto ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Palestra</p>
+                                            {nelleVicinanze.palestra ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Cibo da asporto</p>
+                                            {nelleVicinanze.cibo_da_asporto ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.houseInfoInnerBriefDetailsContainer} style={{ paddingLeft: "30px" }}>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Biblioteca</p>
+                                            {nelleVicinanze.biblioteca ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Supermercati</p>
+                                            {nelleVicinanze.supermercati ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Parchi</p>
+                                            {nelleVicinanze.parchi ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Campo sportivo</p>
+                                            {nelleVicinanze.campo_sportivo ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                        <div className={styles.houseDetailItem}>
+                                            <p>Luoghi di culto</p>
+                                            {nelleVicinanze.luoghi_di_culto ? (
+                                                <Image src={"/check.svg"} width={20} height={20} />
+                                            ) : (
+                                                <Image src={"/cross.svg"} width={20} height={20} />
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    ) : (
+                        <div className={styles.customContainer}>
+                            <div className={styles.pageNotFoundContainer}>
+                                <div style={{ marginTop: "200px" }}>
+                                    <h1>L'immobile che stai cercando non è attualmente più disponibile.</h1>
+                                </div>
+                                <div style={{ marginTop: "50px" }}>
+                                    <CustomButton text="TORNA AGLI IMMOBILI" path="./immobili" theme="dark" />
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </section>
 
                 <section className={styles.linkedFeaturedHouses}>
